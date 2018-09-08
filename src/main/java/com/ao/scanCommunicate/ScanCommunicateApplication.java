966cd28dff053511f6a0dbf96f46cb0d7b26d9f9
aohanhe2 <13823904000@139.com>
2018-09-08 11:52:51 +0800

    创建工程

---
+++ src/main/java/com/ao/scanCommunicate/ScanCommunicateApplication.java
@@ -0,0 +1,12 @@
+package com.ao.scanCommunicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanCommunicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanCommunicateApplication.class, args);
+	}
+}
